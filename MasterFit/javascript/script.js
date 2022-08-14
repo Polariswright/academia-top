9d74f379e50c4dbbfe6fa4f65d563393ad9af413
Marcus <108342348+iTech20@users.noreply.github.com>
2022-08-14 08:32:32 -0300

    2.0

---
+++ MasterFit/javascript/script.js
@@ -0,0 +1,11 @@
+
+function dScale(x) {
+    x.style.zoom = '100%';
+    x.style.opacity = '0.9';
+}
+function uScale(x) {
+    x.style.zoom = '150%';
+    x.style.opacity = '1';
+
+}
+let box = document.getElementsByClassName(movingBox);
